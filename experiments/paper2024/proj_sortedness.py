32067f206986e1824b0c6498673f4e2847325bbb
davips <dpsabc@gmail.com>
2023-11-11 17:56:15 -0300

    Add cli flag `best` to only show results

--- experiments/paper2024/proj_sortedness.py
+++ experiments/paper2024/proj_sortedness.py
@@ -38,14 +38,29 @@ datasets = [
     "svhn"
 ]
 datasets = [f"{d}{argv[1]}" for d in datasets]
+onlyshowbest = "best" in argv
+max_evals = int(argv[2])
 with (sopen(schedule_uri) as db, sopen(remote_cache_uri) as remote):
-    for d in Scheduler(db) << datasets:
+    tasks = datasets if onlyshowbest else (Scheduler(db) << datasets)
+    for d in tasks:
         dataset_name = d[:-1]
+        key = f"{dataset_name}a-trials"
+        trials = remote[key] if key in remote else None
+
+        if onlyshowbest:
+            if trials is None:
+                print(f"{dataset_name[:8]:8} has no trials yet")
+                continue
+            dct = {k: round(v[0], 3) if round(v[0]) - v[0] != 0 else int(v[0])
+                   for k, v in trials.best_trial["misc"]["vals"].items()}
+            print(f"{dataset_name[:8]:8} best:", dct, f"λ: {-trials.best_trial['result']['loss']:2.3f}", flush=True)
+            continue
+
         print(d, "---------------------------------------------------------------------------")
-        kwargs = {"trials": remote[key]} if (key := f"{dataset_name}a-trials") in remote else {}
+        kwargs = {} if trials is None else {"trials": remote[key]}
 
         X, y = load_dataset(dataset_name)
-        X_, trials = balanced_embedding__opt(X, symmetric=False, embedding__param_space={"epochs": (1, 50)}, max_evals=15, progressbar=True, show_parameters=True, return_trials=True, **kwargs)
+        X_, trials = balanced_embedding__opt(X, symmetric=False, embedding__param_space={"epochs": (1, 40)}, max_evals=max_evals, progressbar=True, show_parameters=True, return_trials=True, **kwargs)
 
         remote[key] = trials
 
