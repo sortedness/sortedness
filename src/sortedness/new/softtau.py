46f581110b0233058d054f55c9c87beb59a7dc58
davips <dpsabc@gmail.com>
2024-05-14 21:21:07 -0300

    Finished softtau distance

--- src/sortedness/new/softtau.py
+++ src/sortedness/new/softtau.py
@@ -23,66 +23,210 @@
 from math import tanh
 
 
-def softmerge(x0, x1, y0, y1, mx0, mx1, my0, my1, tau=True, lambd=1.0):
+def softmerge(x0, x1, y0, y1, Sx0, Sx1, Sy0, Sy1, estimate="average", tau=True, lambd=1.0):
     """
     sorts on x
 
-    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 15, 6, 15, tau=True, lambd=0.0000001)  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
-    >>> softmerge([1,2,3], [4,5,6], [1,2,3], [4,5,6], 6, 15, 6, 15, tau=True, lambd=0.0000001)  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
-    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 15, 6, 15, lambd=0.0000001)  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
-    >>> softmerge([1,2,3], [4,5,6], [1,2,3], [4,5,6], 6, 15, 6, 15, lambd=0.0000001)  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
-    ([1, 1, 3, 5, 20, 300], [5, 8, 9, 10, 6, 7], 1.3829..., 5, 0, 13, 0)
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="lowest", lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.0
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="average", lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    5.0
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="highest", lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    5.0
 
-    :param x0:
-    :param x1:
-    :param y0:
-    :param y1:
-    :param lambd:
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.04726...
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    4.67889...
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    5.43661...
+
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="lowest", tau=False, lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -13.0
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="average", tau=False, lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -8.0
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="highest", tau=False, lambd=0.0000001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -8.0
+
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="lowest", tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -10.89364...
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="average", tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -6.66168...
+    >>> softmerge([1,2,3], [1,3,5], [5,6,7], [8,9,10], 6, 9, 18, 27, estimate="highest", tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -5.95750...
+
+    :param x0:  Left halt of `x`.
+    :param x1:  Right halt of `x`.
+    :param y0:  Left halt of `y`.
+    :param y1:  Right halt of `y`.
+    :param Sy0: Sum of `y0` values.
+    :param Sy1: Sum of `y1` values.
+    :param Sx0: Sum of `x0` values.
+    :param Sx1: Sum of `x1` values.
+    :param estimate: All options have the same computational cost in practice.
+        * "lowest"  →   most conservative; adopt the first difference as if it was the value for all pairs.
+        * "average" →   most comprehensive for optimization as all items are included in the calculation;
+                        adopt the average difference as if it was the value for all pairs.
+        * "highest" →   most optimistic; adopt the last difference as if it was the value for all pairs;
+                        along with "lowest", useful to estimate bounds.
+    :param tau:     Approximate Kendall tau, instead of focusing on taking into account the magnitude of agreements and disagreements.
+        * True  →   More interpretable.
+        * False →   Best for optimization, if distances are to be somewhow preserved and if ties are causing problems.
+                    The higher the better: (-inf, 0]
+    :param lambd:   Regularizer.
+                    Surrogate function tends to (non differentiable) Kendall tau when `lambd` tends to 0.
     :return:
     """
     if not x0:
-        return x1, y1, 0, mx0, mx1, my0, my1
+        return x1, y1, 0
     if not x1:
-        return x0, y0, 0, mx0, mx1, my0, my1
+        return x0, y0, 0
+    # todo: test for all sorted
     if x0[0] <= x1[0]:
-        mx0_, my0_ = mx0 - x0[0], my0 - y0[0]
-        rx, ry, t, mx0, mx1, my0, my1 = softmerge(x0[1:], x1, y0[1:], y1, mx0_, mx1, my0_, my1, tau, lambd)
         l = len(x1)
-        tanx = tanh((mx1 / l - x0[0]) / lambd)
-        tany = tanh((my1 / l - y0[0]) / lambd)
-        print(tany, f"{my1}/{l} - {y0[0]}")
-        dt = (tanx * tany) if tau else ((tanx - tany) ** 2)
-        return x0[:1] + rx, y0[:1] + ry, t + l * dt, mx0, mx1, my0, my1
+        if estimate == "average":
+            mx, my = Sx1 / l, Sy1 / l
+        elif estimate == "lowest":
+            mx, my = x1[0], y1[0]
+        elif estimate == "highest":
+            mx, my = x1[-1], y1[-1]
+        else:
+            raise Exception(f"Unknown: {estimate=}")
+        tanx = tanh((mx - x0[0]) / lambd)
+        tany = tanh((my - y0[0]) / lambd)
+        dt = (tanx * tany) if tau else -((tanx - tany) ** 2)
+        rx, ry, t = softmerge(x0[1:], x1, y0[1:], y1, Sx0 - x0[0], Sx1, Sy0 - y0[0], Sy1, estimate, tau, lambd)
+        x, y = x0[:1] + rx, y0[:1] + ry
     else:
-        mx1_, my1_ = mx1 - x1[0], my1 - y1[0]
-        rx, ry, t, mx0, mx1, my0, my1 = softmerge(x0, x1[1:], y0, y1[1:], mx0, mx1, my0, my1_, tau, lambd)
         l = len(x0)
-        tanx = tanh((x1[0] - mx0 / l) / lambd)
-        tany = tanh((y1[0] - my0 / l) / lambd)
-        dt = (tanx * tany) if tau else ((tanx - tany) ** 2)
-        return x1[:1] + rx, y1[:1] + ry, t + l * dt, mx0, mx1, my0, my1
+        if estimate == "average":
+            mx, my = Sx0 / l, Sy0 / l
+        elif estimate == "lowest":
+            mx, my = x0[-1], y0[-1]
+        elif estimate == "highest":
+            mx, my = x0[0], y0[0]
+        else:
+            raise Exception(f"Unknown: {estimate=}")
+        tanx = tanh((x1[0] - mx) / lambd)
+        tany = tanh((y1[0] - my) / lambd)
+        dt = (tanx * tany) if tau else -((tanx - tany) ** 2)
+        rx, ry, t = softmerge(x0, x1[1:], y0, y1[1:], Sx0, Sx1 - x1[0], Sy0, Sy1 - y1[0], estimate, tau, lambd)
+        x, y = x1[:1] + rx, y1[:1] + ry
+    return x, y, t + l * dt
 
 
-def sort(x, y, tau=True, lambd=0.1):
+def sort(x, y, estimate="average", tau=True, lambd=1.0):
     """y should be sorted
 
-    >>> sort([1,2,3,4,5], [1,2,3,4,5])
-    ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], 10)
-    >>> sort([2,1,3,4,5], [1,2,3,4,5])
-    ([1, 2, 3, 4, 5], [2, 1, 3, 4, 5], 8)
-    >>> sort([1,1,3,4,5], [1,2,3,4,5])
-    ([1, 1, 3, 4, 5], [1, 2, 3, 4, 5], 10)
-    >>> sort([1,1,3,4,5], [1,1,3,4,5])
-    ([1, 1, 3, 4, 5], [1, 1, 3, 4, 5], 10)
-    >>> sort([1,2,3,4,5], [1,1,3,4,5])
-    ([1, 2, 3, 4, 5], [1, 1, 3, 4, 5], 10)
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="highest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    10.0
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="highest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.0
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="highest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="highest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="highest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
 
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.98512...
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    7.82502...
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.41786...
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.43067...
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="highest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.41786...
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="average", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    10.0
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="average", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.0
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="average", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="average", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="average", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.55708...
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    7.39414...
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.066794947761027
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.15941...
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="average")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.06679...
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="lowest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    10.0
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="lowest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    8.0
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="lowest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="lowest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="lowest", lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    9.0
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    6.848227...
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    5.56523...
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    6.73071...
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    7.31617...
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], estimate="lowest")[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    6.73071...
+
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.0
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -2.32587...
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -0.58291...
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.0
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], tau=False)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -0.58291...
+
+    >>> sort([1,2,3,4,5], [1,2,3,4,5], tau=False, lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.0
+    >>> sort([2,1,3,4,5], [1,2,3,4,5], tau=False, lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -4.0
+    >>> sort([1,1,3,4,5], [1,2,3,4,5], tau=False, lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -1.0
+    >>> sort([1,1,3,4,5], [1,1,3,4,5], tau=False, lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    0.0
+    >>> sort([1,2,3,4,5], [1,1,3,4,5], tau=False, lambd=0.0001)[2]  # doctest:+ELLIPSIS +NORMALIZE_WHITESPACE
+    -1.0
+
+    :param x:           Vector of numbers.
+    :param y:           Ordered vector of numbers.
+    :param estimate:    All options have the same computational cost in practice.
+        * "lowest"  →   most conservative; adopt the first difference as if it was the value for all pairs.
+        * "average" →   most comprehensive for optimization as all items are included in the calculation;
+                        adopt the average difference as if it was the value for all pairs.
+        * "highest" →   most optimistic; adopt the last difference as if it was the value for all pairs;
+                        along with "lowest", useful to estimate bounds.
+    :param tau:     Approximate Kendall tau, instead of focusing on taking into account the magnitude of agreements and disagreements.
+        * True  →   More interpretable.
+        * False →   Best for optimization, if distances are to be somewhow preserved and if ties are causing problems.
+    :param lambd:   Regularizer.
+                    Surrogate function tends to (non differentiable) Kendall tau when `lambd` tends to 0.
+    :return:
     """
     xl, yl = len(x), len(y)
     if xl == 1:
         return x, y, 0, x[0], y[0]
     mid = xl // 2
-    x0, y0, t0, mx0, my0 = sort(x[:mid], y[:mid])
-    x1, y1, t1, mx1, my1 = sort(x[mid:], y[mid:])
-    rx, ry, t, mx0, mx1, my0, my1 = softmerge(x0, x1, y0, y1, mx0, mx1, my0, my1, tau, lambd)
-    return rx, ry, t + t0 + t1, mx0 + mx1, my0 + my1
+    x0, y0, t0, Sx0, Sy0 = sort(x[:mid], y[:mid], estimate, tau, lambd)
+    x1, y1, t1, Sx1, Sy1 = sort(x[mid:], y[mid:], estimate, tau, lambd)
+    rx, ry, t = softmerge(x0, x1, y0, y1, Sx0, Sx1, Sy0, Sy1, estimate, tau, lambd)
+    return rx, ry, t + t0 + t1, Sx0 + Sx1, Sy0 + Sy1
